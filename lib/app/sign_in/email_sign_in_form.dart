175a95880c317a79b3255cdff8d4da1c8e9f20c6
Clarence Ho <cho@fitbit.com>
2020-07-30 10:13:38 +0800

    Started section 11

---
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
+
+class EmailsignInForm extends StatelessWidget {
+
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'test@test.com',
+        ),
+      ),
+      SizedBox(height: 8.0),
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 8.0),
+      FormSubmitButton(
+        text: 'Sign in',
+        onPressed: () {},
+      ),
+      SizedBox(height: 8.0),
+      FlatButton(
+        child: Text('Need an account? Register'),
+        onPressed: () {},
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
@@ -27,6 +28,15 @@ class SignInPage extends StatelessWidget {
     }
   }
 
+  void _signInWithEmail(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        fullscreenDialog: true,
+        builder: (context) => EmailSignInPage(),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,12 +44,12 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: _buildContent(),
+      body: _buildContent(context),
       backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget _buildContent() {
+  Widget _buildContent(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -75,7 +85,7 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with email',
             color: Colors.teal[700],
             textColor: Colors.white,
-            onPressed: () {},
+            onPressed: () => _signInWithEmail(context),
           ),
           SizedBox(height: 8.0),
           Text(
