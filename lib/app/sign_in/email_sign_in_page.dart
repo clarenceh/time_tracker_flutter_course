66d678405b2cf3715dd0450a5c3667e45653d62a
Clarence Ho <cho@fitbit.com>
2020-08-07 18:15:47 +0800

    Finished section 16

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/app/sign_in/email_signin_form_bloc_based.dart';
-
-import 'email_sign_in_form_stateful.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_signin_form_change_notifier.dart';
 
 class EmailSignInPage extends StatelessWidget {
 
@@ -16,7 +14,7 @@ class EmailSignInPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Card(
-            child: EmailSignInFormBlocBased.create(context),
+            child: EmailSignInFormChangeNotifier.create(context),
           ),
         ),
       ),
---
+++ lib/app/sign_in/email_signin_form_change_notifier.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_bloc.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_change_model.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_model.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class EmailSignInFormChangeNotifier extends StatefulWidget {
+  final EmailSignInChangeModel model;
+
+  EmailSignInFormChangeNotifier({@required this.model});
+
+  static Widget create(BuildContext context) {
+    final AuthBase auth = Provider.of<AuthBase>(context, listen: false);
+
+    return ChangeNotifierProvider<EmailSignInChangeModel>(
+      create: (context) => EmailSignInChangeModel(auth: auth),
+      child: Consumer<EmailSignInChangeModel>(
+        builder: (context, model, _) => EmailSignInFormChangeNotifier(model: model),
+      ),
+    );
+  }
+
+  @override
+  _EmailSignInFormChangeNotifierState createState() =>
+      _EmailSignInFormChangeNotifierState();
+}
+
+class _EmailSignInFormChangeNotifierState extends State<EmailSignInFormChangeNotifier> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  EmailSignInChangeModel get model => widget.model;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  Future<void> _submit() async {
+    try {
+      await model.submit();
+      Navigator.of(context).pop();
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(
+        title: 'Sign in failed',
+        exception: e,
+      ).show(context);
+    }
+  }
+
+  void _toggleFormType() {
+    print("toggle form type, model is loading: ${model.isLoading}");
+    model.toggleFormType();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  void _emailEditingComplete() {
+    final nextFocus = model.emailValidator.isValid(model.email)
+        ? _passwordFocusNode
+        : _emailFocusNode;
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      _buildEmailTextField(),
+      SizedBox(height: 8.0),
+      _buildPasswordTextField(),
+      SizedBox(height: 8.0),
+      FormSubmitButton(
+        text: model.primaryButtonText,
+        onPressed: model.canSubmit ? _submit : null,
+      ),
+      SizedBox(height: 8.0),
+      FlatButton(
+        child: Text(model.secondaryButtonText),
+        onPressed: !model.isLoading ? _toggleFormType : null,
+      ),
+    ];
+  }
+
+  TextField _buildPasswordTextField() {
+    return TextField(
+      controller: _passwordController,
+      focusNode: _passwordFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        errorText: model.passwordErrorText,
+        enabled: model.isLoading == false,
+      ),
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      onEditingComplete: _submit,
+      onChanged: model.updatePassword,
+    );
+  }
+
+  TextField _buildEmailTextField() {
+    return TextField(
+      controller: _emailController,
+      focusNode: _emailFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'test@test.com',
+        errorText: model.emailErrorText,
+        enabled: model.isLoading == false,
+      ),
+      autocorrect: false,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      onEditingComplete: () => _emailEditingComplete(),
+      onChanged: model.updateEmail,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
