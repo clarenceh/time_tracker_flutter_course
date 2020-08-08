3d5e67fdf09ed65a33e5766510cd08121aa4f9c3
clarenceh <ho.clarence@gmail.com>
2020-08-08 18:33:24 +0800

    Progressing section 17

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
 
-class HomePage extends StatelessWidget {
+class JobsPage extends StatelessWidget {
 
   Future<void> _signOut(BuildContext context) async {
     try {
@@ -27,11 +28,20 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  Future<void> _createJob(BuildContext context) async {
+    final database = Provider.of<Database>(context, listen: false);
+
+    await database.createJob({
+      'name': 'Blogging',
+      'ratePerHour': 10,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Jobs'),
         actions: [
           FlatButton(
             child: Text(
@@ -45,6 +55,11 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _createJob(context),
+      ),
     );
   }
+
 }
